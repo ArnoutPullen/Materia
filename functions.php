3e0471f772de633391ffb2cc14c330b02055c194
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2017-11-23 21:05:45 +0100

    Added jquery

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'menus' );
-// Theme Support 
-// function mat_register_nav_menu() {
-//     register_nav_menu('header-menu',__( 'Header Menu' ));
-//   }
-//   add_action( 'init', 'mat_register_nav_menu' );
+
+function materia_enqueue_script() {
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/framework/js/jquery-3.2.1.min.js', array( 'jquery' ), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'materia_enqueue_script' );
+
